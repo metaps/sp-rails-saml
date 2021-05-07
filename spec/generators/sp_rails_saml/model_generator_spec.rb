76d13c9dfaaa3e367ede4f7666065326e5cbf978
Hiroto Yonetani <35982148+Sibakeny@users.noreply.github.com>
2021-05-07 17:05:25 +0900

    モデルのgeneratorに関するテストの作成 (#3)
    Co-authored-by: yonetani <hiroto_yonetani@metaps.com>

---
+++ spec/generators/sp_rails_saml/model_generator_spec.rb
@@ -0,0 +1,29 @@
+RSpec.describe SpRailsSaml::ModelGenerator, type: :generator do
+  destination File.expand_path("../../../../tmp", __FILE__)
+  arguments %w(account)
+
+  before(:all) do
+    prepare_destination
+    run_generator
+  end
+
+  let(:migration_text) do
+<<-EOS
+class CreateSamlSettings < ActiveRecord::Migration[6.1]
+  def change
+    create_table :saml_settings do |t|
+      t.references :account, foreign_key: true, null: false
+      t.string :idp_sso_url
+      t.text   :idp_cert
+      t.string :idp_entity_id
+      t.timestamps
+    end
+  end
+end
+EOS
+  end
+
+  it "should create saml_settings migration file" do
+    assert_file "db/migrations/timestamp_create_saml_settings.rb", migration_text
+  end
+end
