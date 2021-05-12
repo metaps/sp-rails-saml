9e6b1673681f4a5fba8f5dede6c38a5170fffcce
psyashes <43512814+psyashes@users.noreply.github.com>
2021-05-12 17:57:40 +0900

    Add comment to migration file

--- lib/generators/sp_rails_saml/templates/create_saml_settings.rb
+++ lib/generators/sp_rails_saml/templates/create_saml_settings.rb
@@ -1,10 +1,10 @@
 class CreateSamlSettings < ActiveRecord::Migration[6.1]
   def change
-    create_table :saml_settings do |t|
-      t.references :account, foreign_key: true, null: false
-      t.string :idp_sso_url
-      t.text   :idp_cert
-      t.string :idp_entity_id
+    create_table :saml_settings, comment: 'Saml settings table for sp' do |t|
+      t.references :account, foreign_key: true, null: false, comment: 'For company account model'
+      t.string :idp_sso_url, comment: 'URL for Idp SSO'
+      t.text   :idp_cert, comment: 'X.509 Certification of Idp'
+      t.string :idp_entity_id, comment: 'Entity ID of Idp'
       t.timestamps
     end
   end
