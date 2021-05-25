36863d1925ca9de23fe1118d649097bac9982c82
psyashes <43512814+psyashes@users.noreply.github.com>
2021-05-25 15:21:44 +0900

    Fix initializer generator

--- lib/generators/sp-rails-saml/config_generator.rb
+++ lib/generators/sp-rails-saml/config_generator.rb
@@ -1,26 +1,27 @@
 require 'rails/generators'
 
 module SpRailsSaml
+  # Initializer file Generator.
+  #
   class ConfigGenerator < Rails::Generators::Base
-
-    desc "config/initializersにイニシャライザファイルを作成します"
+    desc 'Generate sp-rails-saml.rb to config/initializers'
 
     def create_initializer_file
-      create_file "config/initializers/sp-rails-saml.rb", default_initializer
+      create_file 'config/initializers/sp-rails-saml.rb', default_initializer
     end
 
     private
 
     def default_initializer
-<<-EOS
-SpRailsSaml::Settings.setup do |config|
-  config.sp_entity_id = ''
-  config.name_identifier_format = 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress'
-  config.authn_context = 'urn:oasis:names:tc:SAML:2.0:ac:classes:X509'
-  config.authn_context_comparison = 'exact'
-  config.assertion_consumer_service_url = ''
-end
-EOS
+      <<~RUBY
+        SpRailsSaml::Settings.setup do |config|
+          config.sp_entity_id = ''
+          config.name_identifier_format = 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress'
+          config.authn_context = 'urn:oasis:names:tc:SAML:2.0:ac:classes:X509'
+          config.authn_context_comparison = 'exact'
+          config.assertion_consumer_service_url = ''
+        end
+      RUBY
     end
   end
 end
