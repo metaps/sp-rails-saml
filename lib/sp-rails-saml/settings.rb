3bf7f496bddd1cea1b28ce62c5519525f3bb668c
psyashes <43512814+psyashes@users.noreply.github.com>
2021-05-26 19:29:46 +0900

    Fix attribute

--- lib/sp-rails-saml/settings.rb
+++ lib/sp-rails-saml/settings.rb
@@ -9,11 +9,11 @@ module SpRailsSaml
     }.freeze
 
     class << self
-      attr_accessor :sp_entity_id
-      attr_accessor :name_identifier_format
-      attr_accessor :authn_context
-      attr_accessor :authn_context_comparison
-      attr_accessor :assertion_consumer_service_url
+      attr_accessor :sp_entity_id,
+                    :name_identifier_format,
+                    :authn_context,
+                    :authn_context_comparison,
+                    :assertion_consumer_service_url
 
       def setup(options = {})
         options.each do |key, value|
