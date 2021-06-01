09f327755ae6d5c2ff3ec5ba39ae52d505ca5f8b
psyashes <43512814+psyashes@users.noreply.github.com>
2021-06-01 21:27:55 +0900

    Add generator path

--- lib/sp_rails_saml.rb
+++ lib/sp_rails_saml.rb
@@ -3,6 +3,7 @@ require 'sp-rails-saml/settings'
 require 'sp-rails-saml/authnrequest'
 require 'sp-rails-saml/saml_response'
 require 'generators/sp-rails-saml/config_generator'
+require 'generators/sp-rails-saml/sp_rails_saml_controller_generator'
 
 module SpRailsSaml
   class Error < StandardError; end
