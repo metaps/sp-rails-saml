8883024495a277049d6d5ef5e62b808348c66fd6
psyashes <43512814+psyashes@users.noreply.github.com>
2021-06-01 21:39:09 +0900

    Use inline ruby code instead of template file

--- lib/generators/sp-rails-saml/sp_rails_saml_controller_generator.rb
+++ lib/generators/sp-rails-saml/sp_rails_saml_controller_generator.rb
@@ -9,7 +9,40 @@ module SpRailsSaml
     source_root File.expand_path('templates', __dir__)
 
     def create_controller_file
-      create_file 'app/controllers/sp_rails_saml_controller.rb', 'sp_rails_saml_controller.rb'
+      create_file 'app/controllers/sp_rails_saml_controller.rb', <<~RUBY
+        module SpRailsSaml
+          # Controller to register saml by SP
+          #
+          class SpRailsSamlController < ApplicationController
+            # GET /saml_settings
+            #
+            def show
+              account = Account.find_by(id: params[:account_id])
+              @saml_setting = account.saml_setting
+            end
+
+            # GET /saml_settings/edit
+            #
+            def edit
+              account = Account.find_by(id: params[:account_id])
+              @saml_setting = account.saml_setting
+            end
+
+            # PATCH /saml_settings
+            #
+            def update
+              account = Account.find_by(id: params[:account_id])
+              @saml_setting = account.saml_setting
+
+              if @saml_setting.update(saml_setting_params)
+                redirect_to saml_setting_path
+              else
+                render :edit
+              end
+            end
+          end
+        end
+      RUBY
     end
   end
 end
