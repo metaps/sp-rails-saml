9c553616411237abe7e50f2e2ac84e5cc44eec59
yonetani <hiroto_yonetani@metaps.com>
2021-06-01 09:51:57 +0900

    rubocopの警告の修正

--- lib/generators/sp-rails-saml/model_generator.rb
+++ lib/generators/sp-rails-saml/model_generator.rb
@@ -5,34 +5,34 @@ module SpRailsSaml
   class ModelGenerator < ActiveRecord::Generators::Base
     include Rails::Generators::Migration
 
-    source_root File.expand_path("../templates", __FILE__)
+    source_root File.expand_path('templates', __dir__)
 
-    default_migration_file = <<-EOS
-class CreateSamlSettings < ActiveRecord::Migration[6.1]
-  def change
-    create_table :saml_settings, comment: 'Saml settings table for sp' do |t|
-      t.references :account, foreign_key: true, null: false, comment: 'For company account model'
-      t.string :idp_sso_url, comment: 'URL for Idp SSO'
-      t.text   :idp_cert, comment: 'X.509 Certification of Idp'
-      t.string :idp_entity_id, comment: 'Entity ID of Idp'
-      t.timestamps
-    end
-  end
-end
-EOS
-    
+    default_migration_file = <<~RUBY
+      class CreateSamlSettings < ActiveRecord::Migration[6.1]
+        def change
+          create_table :saml_settings, comment: 'Saml settings table for sp' do |t|
+            t.references :account, foreign_key: true, null: false, comment: 'For company account model'
+            t.string :idp_sso_url, comment: 'URL for Idp SSO'
+            t.text   :idp_cert, comment: 'X.509 Certification of Idp'
+            t.string :idp_entity_id, comment: 'Entity ID of Idp'
+            t.timestamps
+          end
+        end
+      end
+    RUBY
+
     # Overwrite default migration file for arguments
-    File.write("./lib/generators/sp_rails_saml/templates/create_saml_settings.rb", default_migration_file)
+    File.write('./lib/generators/sp-rails-saml/templates/create_saml_settings.rb', default_migration_file)
 
     def copy_migration
-      migration_template "create_saml_settings.rb", "db/migrate/create_saml_settings.rb"
+      migration_template 'create_saml_settings.rb', 'db/migrate/create_saml_settings.rb'
     end
 
     def copy_model
-      create_file "app/models/saml_setting.rb", <<-FILE
-class SamlSetting < ApplicationRecord
-end
-    FILE
+      create_file 'app/models/saml_setting.rb', <<~FILE
+        class SamlSetting < ApplicationRecord
+        end
+      FILE
     end
   end
 end
