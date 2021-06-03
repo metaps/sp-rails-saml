b5b4ebb8be12718f8a737e80371604fd773e7574
yonetani <hiroto_yonetani@metaps.com>
2021-06-03 19:05:45 +0900

    userが存在しない場合はログイン画面を再度表示する

--- app/controllers/saml/sessions_base_controller.rb
+++ app/controllers/saml/sessions_base_controller.rb
@@ -8,6 +8,12 @@ module Saml
     # POST /saml/sign_in
     def create
       user = SpRailsSaml::Settings.user_class.find_by(email: params[:email])
+
+      if user.blank?
+        redirect_to saml_sign_in_path, alert: 'failed to login'
+        return
+      end
+
       account = user.send(SpRailsSaml::Settings.account_class.to_s.downcase.to_sym)
 
       authnrequest = SpRailsSaml::Authnrequest.new(account.saml_setting).to_url
