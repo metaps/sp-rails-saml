fc136a928b830f87e2afb74840dfea159c4ed751
psyashes <43512814+psyashes@users.noreply.github.com>
2021-06-07 23:47:20 +0900

    Add saml setting routing

--- lib/sp-rails-saml/routes/routes_template.rb
+++ lib/sp-rails-saml/routes/routes_template.rb
@@ -1,4 +1,10 @@
 namespace :saml do
+  # Session
   get 'sign_in', to: 'sessions#new'
   post 'sign_in', to: 'sessions#create'
+
+  # Saml settings for SP
+  get 'saml_setting', to: 'saml_setting#show'
+  get 'saml_setting', to: 'saml_setting#edit'
+  patch 'saml_setting', to: 'saml_setting#edit'
 end
