b95e9dc85d5302cf40a2417e272b82fb7f650aa5
psyashes <43512814+psyashes@users.noreply.github.com>
2021-06-08 13:00:47 +0900

    Modify views generators

--- lib/generators/sp-rails-saml/views_generator.rb
+++ lib/generators/sp-rails-saml/views_generator.rb
@@ -4,10 +4,11 @@ module SpRailsSaml
   class ViewsGenerator < Rails::Generators::Base
     source_root File.expand_path('../templates', __dir__)
 
-    desc 'view関係のファイルを自動生成します。'
+    desc 'Generate view files.'
 
     def create
       copy_file 'views/sessions/new.html.erb', 'app/views/saml/sessions/new.html.erb'
+      copy_file 'views/saml/new.html.erb', 'app/views/saml/new.html.erb'
     end
   end
 end
