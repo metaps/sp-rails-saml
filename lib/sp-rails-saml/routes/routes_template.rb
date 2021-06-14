eca85553ee0de815ec7e79734e59c5f53bfc9692
psyashes <43512814+psyashes@users.noreply.github.com>
2021-06-14 21:35:41 +0900

    Remove edit endpoint

--- lib/sp-rails-saml/routes/routes_template.rb
+++ lib/sp-rails-saml/routes/routes_template.rb
@@ -5,8 +5,7 @@ namespace :saml do
 
   # Saml settings for SP
   get 'saml_settings', to: 'saml_settings#show'
-  get 'saml_settings', to: 'saml_settings#edit'
-  patch 'saml_settings', to: 'saml_settings#edit'
+  patch 'saml_settings', to: 'saml_settings#update'
 
   # SSO
   post 'sso/:id', to: 'ssos#consume', as: :sso
