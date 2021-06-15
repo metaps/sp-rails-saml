a05b6562e6c548f444f9050e9d19474d24e820b5
psyashes <43512814+psyashes@users.noreply.github.com>
2021-06-15 20:58:52 +0900

    Separete view generator method

--- lib/generators/sp-rails-saml/views_generator.rb
+++ lib/generators/sp-rails-saml/views_generator.rb
@@ -6,8 +6,11 @@ module SpRailsSaml
 
     desc 'Generate view files.'
 
-    def create
+    def create_session_view
       copy_file 'views/sessions/new.html.erb', 'app/views/saml/sessions/new.html.erb'
+    end
+
+    def create_saml_setting_view
       copy_file 'views/saml/new.html.erb', 'app/views/saml/new.html.erb'
     end
   end
