e236821b538d8868d288e6242b28af53de551196
psyashes <43512814+psyashes@users.noreply.github.com>
2021-06-16 15:23:49 +0900

    Fix Style/StringLiterals

--- spec/generators/sp_rails_saml/views_generator_spec.rb
+++ spec/generators/sp_rails_saml/views_generator_spec.rb
@@ -1,13 +1,13 @@
 RSpec.describe SpRailsSaml::ViewsGenerator, type: :generator do
-  destination File.expand_path("../../../../tmp", __FILE__)
+  destination File.expand_path('../../../../tmp', __FILE__)
 
   before(:all) do
     prepare_destination
     run_generator
   end
 
-  it "should create views" do
-    assert_file "app/views/saml/sessions/new.html.erb", file_fixture('views/sessions/new.html.erb')
-    assert_file "app/views/saml/new.html.erb", file_fixture('views/saml/new.html.erb')
+  it 'should create views' do
+    assert_file 'app/views/saml/sessions/new.html.erb', file_fixture('views/sessions/new.html.erb')
+    assert_file 'app/views/saml/new.html.erb', file_fixture('views/saml/new.html.erb')
   end
 end
