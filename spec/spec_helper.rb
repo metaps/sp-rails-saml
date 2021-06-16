9f8cb9a1a43429e22bbdf6c5aaa6766ddc2fe2de
psyashes <43512814+psyashes@users.noreply.github.com>
2021-06-16 20:59:55 +0900

    Fix Style/StringLiterals

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,14 @@
 require 'ostruct'
 require 'rexml/document'
-require "bundler/setup"
-require "sp-rails-saml"
-require "generator_spec"
+require 'bundler/setup'
+require 'sp-rails-saml'
+require 'generator_spec'
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].sort.each { |f| require f }
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
