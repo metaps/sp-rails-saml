83f0c438cd662b66897f228288f8de75460a6626
psyashes <43512814+psyashes@users.noreply.github.com>
2021-06-16 15:21:53 +0900

    Use migration template

--- spec/generators/sp_rails_saml/model_generator_spec.rb
+++ spec/generators/sp_rails_saml/model_generator_spec.rb
@@ -7,24 +7,8 @@ RSpec.describe SpRailsSaml::ModelGenerator, type: :generator do
     run_generator
   end
 
-  let(:migration_text) do
-    <<~RUBY
-      class CreateSamlSettings < ActiveRecord::Migration[6.1]
-        def change
-          create_table :saml_settings, comment: 'Saml settings table for sp' do |t|
-            t.references :account, foreign_key: true, null: false, comment: 'For company account model'
-            t.string :idp_sso_url, comment: 'URL for Idp SSO'
-            t.text   :idp_cert, comment: 'X.509 Certification of Idp'
-            t.string :idp_entity_id, comment: 'Entity ID of Idp'
-            t.timestamps
-          end
-        end
-      end
-    RUBY
-  end
-
   it 'should create saml_settings migration file' do
-    assert_migration 'db/migrate/create_saml_settings', migration_text
+    assert_migration 'db/migrate/create_saml_settings', file_fixture('migrate/create_saml_settings.rb')
   end
 
   it 'should create saml_settings model file' do
