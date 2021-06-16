7b5aedbf23d77b6d13fbceef004513f129412393
psyashes <43512814+psyashes@users.noreply.github.com>
2021-06-16 15:10:03 +0900

    Fix Layout/HeredocIndentation

--- spec/generators/sp_rails_saml/settings_generator_spec.rb
+++ spec/generators/sp_rails_saml/settings_generator_spec.rb
@@ -7,15 +7,15 @@ RSpec.describe SpRailsSaml::ConfigGenerator, type: :generator do
   end
 
   let(:initializer_text) do
-<<-EOS
-SpRailsSaml::Settings.setup do |config|
-  config.name_identifier_format         = 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress'
-  config.authn_context                  = 'urn:oasis:names:tc:SAML:2.0:ac:classes:X509'
-  config.authn_context_comparison       = 'exact'
-  config.user_class = User
-  config.account_class = Account
-end
-EOS
+    <<~RUBY
+      SpRailsSaml::Settings.setup do |config|
+        config.name_identifier_format         = 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress'
+        config.authn_context                  = 'urn:oasis:names:tc:SAML:2.0:ac:classes:X509'
+        config.authn_context_comparison       = 'exact'
+        config.user_class = User
+        config.account_class = Account
+      end
+    RUBY
   end
 
   it 'should create saml_settings initializer file' do
