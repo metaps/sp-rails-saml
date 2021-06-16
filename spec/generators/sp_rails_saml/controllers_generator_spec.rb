24257d77ddcbdab3126425ea1e731905a1a3e4b5
psyashes <43512814+psyashes@users.noreply.github.com>
2021-06-16 14:21:16 +0900

    Fix Style/StringLiterals:

--- spec/generators/sp_rails_saml/controllers_generator_spec.rb
+++ spec/generators/sp_rails_saml/controllers_generator_spec.rb
@@ -1,17 +1,17 @@
 RSpec.describe SpRailsSaml::ControllersGenerator, type: :generator do
-  destination File.expand_path("../../../../tmp", __FILE__)
+  destination File.expand_path('../../../../tmp', __FILE__)
 
   before(:all) do
     prepare_destination
     run_generator
   end
 
-  it "should create saml_settings initializer file" do
-    assert_file "app/controllers/saml/sessions_controller.rb", file_fixture('controllers/sessions_controller.rb')
-    assert_file "app/controllers/saml/ssos_controller.rb", file_fixture('controllers/ssos_controller.rb')
+  it 'should create saml_settings initializer file' do
+    assert_file 'app/controllers/saml/sessions_controller.rb', file_fixture('controllers/sessions_controller.rb')
+    assert_file 'app/controllers/saml/ssos_controller.rb', file_fixture('controllers/ssos_controller.rb')
   end
 
-  it "should create saml_settings_controller in app/controllers/saml" do
-    assert_file "app/controllers/saml/saml_settings_controller.rb", file_fixture('controllers/saml_settings_controller.rb')
+  it 'should create saml_settings_controller in app/controllers/saml' do
+    assert_file 'app/controllers/saml/saml_settings_controller.rb', file_fixture('controllers/saml_settings_controller.rb')
   end
 end
